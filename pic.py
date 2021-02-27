1788d2035e49816ee7ceb4b89080adeea957c132
nixiy <mavericks.diva.desire@gmail.com>
2021-02-28 04:38:56 +0900

    [add] Exif解析関数を追加 - 撮影シーンタイプ - 色空間 - 個別画像処理

--- pic.py
+++ pic.py
@@ -32,6 +32,41 @@ def decodeWhiteBalance(key):
     else:
         return None
 
+# 撮影シーンタイプを変換する
+def decodeSceneCaptureType(key):
+    sceneCaptureType = {
+        0:"標準",
+        1:"風景",
+        2:"人物",
+        3:"夜景",
+    }
+    if key in sceneCaptureType:
+        return sceneCaptureType[key]
+    else:
+        return None    
+
+# 色空間を変換する
+def decodeColorSpace(key):
+    colorSpace = {
+        1:"sRGB",
+        0xFFFF:"Uncalibrated"
+    }
+    if key in colorSpace:
+        return colorSpace[key]
+    else:
+        return None
+
+# 個別画像処理
+def decodeCustomRendered(key):
+    customRendered = {
+        0:"通常処理",
+        1:"特殊処理",
+    }
+    if key in customRendered:
+        return customRendered[key]
+    else:
+        return None
+
 def main():
     imagePath = "./resource/test.JPG"
     im:MpoImageFile = Image.open(imagePath)
@@ -51,6 +86,7 @@ def main():
     print("Version:➤➤➤", exifDic["Software"])
 
     print("解像度:➤➤➤",exifDic["ExifImageWidth"],"x",exifDic["ExifImageHeight"])
+    print("35mm焦点距離:➤➤➤",exifDic["FocalLengthIn35mmFilm"],"mm")
     print("ISO:➤➤➤", exifDic["ISOSpeedRatings"])
     print("絞り値:➤➤➤ f",exifDic["FNumber"][0]/exifDic["FNumber"][1])
     print("露出時間:➤➤➤", exifDic["ExposureTime"][0],"/",exifDic["ExposureTime"][1])
@@ -58,6 +94,9 @@ def main():
     print("焦点距離:➤➤➤", exifDic["FocalLength"][0]/exifDic["FocalLength"][1],"mm")
     print("コントラスト:➤➤➤", decodeContrast(exifDic["Contrast"]))
     print("ホワイトバランス:➤➤➤", decodeWhiteBalance(exifDic["WhiteBalance"]))
+    print("撮影シーン:➤➤➤", decodeSceneCaptureType(exifDic["SceneCaptureType"]))
+    print("色空間:➤➤➤", decodeColorSpace(exifDic["ColorSpace"]))
+    print("個別画像処理:➤➤➤", decodeCustomRendered(exifDic["CustomRendered"]))
     print("############# GRiii #############")
 
 if __name__ == "__main__":
